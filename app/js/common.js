ee5452049bba5c2ba54e77e7c5e073338539399f
Alex <PR1ME-GOP@yandex.ru>
2019-03-14 17:25:13 +0400

    Добавил owl карусель, и стартовый шаблон для нее

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,19 @@
-$(function() {      //Для интеграции JQuerry
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        dots: false,
+        items: 1,
+        center: true,
+    });
 
+    var owl = $('.owl-carousel');
+    owl.owlCarousel();
+
+    $('.next').click(function() {
+        owl.trigger('next.owl.carousel');
+    });
+
+    $('.prev').click(function() {
+        owl.trigger('prev.owl.carousel', [300]);
+    });
 });
