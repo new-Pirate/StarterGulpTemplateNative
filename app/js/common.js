3c948d6a243d388b3bed61bb69209dcbab5cbd2a
Alex <PR1ME-GOP@yandex.ru>
2019-07-18 21:22:29 +0400

    Добавил валидацию формы

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
+
+    /* Карусель */
+
     $('.owl-carousel').owlCarousel({
         loop: true,
         dots: false,
@@ -16,4 +19,9 @@ $(document).ready(function () {
     $('.prev').click(function() {
         owl.trigger('prev.owl.carousel', [300]);
     });
+
+    /* Валидация */
+
+    $('#phone').mask('+0 (000) 000-00-00');
+    $('#growth').mask('000');
 });
